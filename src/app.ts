1287d173130f4da37312078180da96e3c5d45a7f
MatheusFerreiraL <matheusfleandro@gmail.com>
2024-01-25 05:31:26 -0300

    Configuração da DB com postgres e docker

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,13 @@
+import { PrismaClient } from '@prisma/client';
 import fastify from 'fastify';
 
 export const app = fastify();
+
+const prisma = new PrismaClient();
+
+await prisma.user.create({
+  data: {
+    name: 'Marcela Ferraz',
+    email: 'mferraz@email.com',
+  },
+});
